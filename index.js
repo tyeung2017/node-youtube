7e0de0a585e098e4a7cf84af2f87dbaa05e45a16
Leo Yeung <leo42us@gmail.com>
2019-07-23 00:01:28 +0100

    change console.log

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const app = express();
 
 const PORT = process.env.PORT || 9001;
 
-app.listen(PORT, () => console.log("hi"));
+app.listen(PORT, () => console.log(`Hi Leo, magic is happening at PORT ${PORT}`));
