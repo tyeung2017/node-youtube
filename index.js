e57ff170106150891a07c72ef44887e3e9cbad72
leo <leo42us@gmail.com>
2019-07-23 23:16:55 +0100

    hook up to index

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+
 const app = express();
 
+const download = require('./download');
+
 const PORT = process.env.PORT || 9001;
+
+const url = 'https://www.youtube.com/watch?v=IeAbAHZX4Rg&list=RDIeAbAHZX4Rg';
+
+download(url);
 
 app.listen(PORT, () => console.log(`Hi Leo, magic is happening at PORT ${PORT}`));
