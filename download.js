c6a677bb5cd33fdc7b814e0067de17f014dce756
leo <leo42us@gmail.com>
2019-07-24 01:10:03 +0100

    update download module for audio only

--- download.js
+++ download.js
@@ -1,7 +1,28 @@
-const fs = require('fs');
 const ytdl = require('ytdl-core');
 
-const download = url => ytdl(url)
-  .pipe(fs.createWriteStream('video.mp4'));
+const readline = require('readline');
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+const start = Date.now();
+const download = (url, name) => {
+  const stream = ytdl(url, {
+    quality: 'highestaudio',
+    filter: 'audioonly',
+  });
+
+  ffmpeg(stream)
+    .audioBitrate(128)
+    .save(`${__dirname}/${name}.mp3`)
+    .on('progress', (p) => {
+      readline.cursorTo(process.stdout, 0);
+      process.stdout.write(`${p.targetSize}kb downloaded`);
+    })
+    .on('end', () => {
+      console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
+    });
+};
 
 module.exports = download;
